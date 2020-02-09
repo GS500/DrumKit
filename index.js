a97fff05b97e904fa0891f5c0b0d910f66fe10ed
GS500 <44592930+GS500@users.noreply.github.com>
2020-02-08 22:17:54 -0600

    Fixed click event

--- index.js
+++ index.js
@@ -3,47 +3,47 @@ let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    let buttonInnerHTML = this.innerHTML;
+    let buttonInnerHTML = this.innerHTML.toUpperCase();
       Keypress(buttonInnerHTML);
   });
 }
 
 //detecting key press
 document.addEventListener("keydown", function(event){
-  Keypress(event.key);
+  Keypress(event.key.toUpperCase());
 });
 
 
 function Keypress(key) {
   switch (key) {
-    case "w":
+    case "W":
       let tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
 
-    case "a":
+    case "A":
       let tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
-    case "s":
+    case "S":
       let tom3 = new Audio("sounds/tom-3.mp3");
       tom3.play();
       break;
-    case "d":
+    case "D":
       let tom4 = new Audio("sounds/tom-4.mp3");
       tom4.play();
       break;
-    case "j":
+    case "J":
       let snare = new Audio("sounds/snare.mp3");
       snare.play();
       break;
 
-    case "k":
+    case "K":
       let crash = new Audio("sounds/crash.mp3");
       crash.play();
       break;
 
-    case "l":
+    case "L":
       let kick = new Audio("sounds/kick-bass.mp3");
       kick.play();
       break;
